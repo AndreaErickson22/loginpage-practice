9e9d7d526e052440e676abc6cd7ac0d6774332d0
Andrea <avertrees@yahoo.com>
2019-05-07 16:18:33 -0600

    practicing-loginpages

---
+++ app/main.js
@@ -0,0 +1,8 @@
+function check(form) {
+  if (form.userid.value == "dew" && form.pswrd.value == "123") {
+    window.open('https://andreaerickson22.github.io/AndreaErickson22/')
+  }
+  else {
+    alert("The username and password you entered do not match")
+  }
+}
